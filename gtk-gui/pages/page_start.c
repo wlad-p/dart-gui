54dbd3742aad8793e45894c4a4aa440e13c4c5ba
Wlad Pesotsky <wladpesotskyy@gmail.com>
2023-11-20 23:28:52 +0100

    css added

--- gtk-gui/pages/page_start.c
+++ gtk-gui/pages/page_start.c
@@ -11,6 +11,11 @@ static void open_menu(GtkWidget *widget, gpointer data) {
 GtkWidget *create_page_start(GtkWidget *window, GtkWidget *stack){
 	GtkWidget *grid = gtk_grid_new();
 
+	GtkCssProvider *cssProvider = gtk_css_provider_new();
+	gtk_css_provider_load_from_path(cssProvider, "start.css", NULL);
+	GtkStyleContext *styleContext = gtk_widget_get_style_context(window);
+	gtk_style_context_add_provider(styleContext, GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+
 	GtkWidget *label1 = gtk_label_new("Wlarts");
 	GtkWidget *label2 = gtk_label_new(" ");
 	gtk_grid_attach(GTK_GRID(grid), label1, 0,0,1,1);
@@ -18,6 +23,7 @@ GtkWidget *create_page_start(GtkWidget *window, GtkWidget *stack){
 
 	//Button
 	GtkWidget *btn = gtk_button_new_with_label("Start");
+	gtk_widget_set_class(GTK_WIDGET(btn), "btn");
 	g_signal_connect(btn, "clicked", G_CALLBACK(open_menu),stack);
 	//gtk_container_add(GTK_CONTAINER(window),btn);
 	gtk_grid_attach(GTK_GRID(grid), btn, 0, 2, 1,1);
